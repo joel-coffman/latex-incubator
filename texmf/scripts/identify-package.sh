db2fdb891322240ada159254aae8fe0d5c9a291d
Joel Coffman <joel.coffman@usafa.edu>
2022-04-19 04:28:31 -0600

    Indent heredoc for readability

--- texmf/scripts/identify-package.sh
+++ texmf/scripts/identify-package.sh
@@ -5,14 +5,14 @@ set -u
 set -x
 
 
-docker run --interactive --rm debian:latest /bin/bash <<SCRIPT
-set -x
+docker run --interactive --rm debian:latest /bin/bash <<-SCRIPT
+	set -x
 
-apt update --quiet
-apt install --no-install-recommends --quiet --yes apt-file
+	apt update --quiet
+	apt install --no-install-recommends --quiet --yes apt-file
 
-apt-file update
-for file in $*; do
-  apt-file search "\$file"
-done
+	apt-file update
+	for file in $*; do
+	  apt-file search "\$file"
+	done
 SCRIPT
